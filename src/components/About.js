e59c2d343177338241b6c8d04aa9bdfecdc9aab6
Dakota Vikdal <dakota.james19@hotmail.com>
2024-01-08 19:20:53 -0500

    Updating

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div id= "about">
-    <h2>About Me</h2>
-    <p>It is irrefutable, videogames are life.</p>
-    <img src= {image} alt= 'I made this'></img>
-    </div>;
+  return(
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Arent I cool</p>
+      <img src={image} alt="I made this"/>
+    </div>
+  )
 }
 
 export default About;
